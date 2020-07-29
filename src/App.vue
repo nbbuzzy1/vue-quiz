568948282ca3e0814f497fdc1a1beaac1518b5e3
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-07-29 06:52:41 -0400

    Set up initial components.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header />
+        <b-container class="bv-example-row">
+            <b-row>
+                <b-col sm="6" offset="3">
+                    <QuestionBox />
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import axios from 'axios'
+    import Header from './components/Header.vue'
+    import QuestionBox from './components/QuestionBox.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            QuestionBox
+        },
+        data() {
+            return {
+                questions: []
+            }
+        },
+        async mounted() {
+            const response = await axios.get('https://opentdb.com/api.php?amount=10')
+            const quizData = response.data.results;
+            console.log(quizData)
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,6 @@
+<template>
+  <b-nav tabs>
+    <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+    <b-nav-item disabled>Counter 4 / 10</b-nav-item>
+  </b-nav>
+</template>
